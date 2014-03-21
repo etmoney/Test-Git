79afb85715d9ad493c1629caef67ec1ce2c917a1
etmoney <etmoney1@naver.com>
2014-03-21 19:45:53 +0900

    Define Max Number

--- Random_Array.c
+++ Random_Array.c
@@ -2,27 +2,29 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define MAX_N 100
+
 int main()
 {
- char array[101] ={0,};
+ char array[MAN_N + 1] ={0,};
  char i;
  char i_rand_data;
  char freq_number = 0;
  srand((unsigned)time(NULL));
 
- for( i = 0 ; i < 101 ; i++ )
+ for( i = 0 ; i < MAX_N + 1 ; i++ )
  {
-	 i_rand_data = (rand()%101);
+	 i_rand_data = (rand()% MAX_N + 1);
 	 array[i_rand_data]++;
  };
 
- for( i = 1 ; i < 101 ; i++ )
+ for( i = 1 ; i < MAX_N + 1 ; i++ )
    if( array[freq_number] <=  array[i] )
 	freq_number = i;
 
  printf("Most frequently number is %d \n", freq_number);
  
- for( i = 0 ; i <101 ; i++ )
+ for( i = 0 ; i <MAX_N + 1 ; i++ )
  {
    if (i % 10 != 9 )
       printf("[ %2d ] = %d, ", i , array[i]);
